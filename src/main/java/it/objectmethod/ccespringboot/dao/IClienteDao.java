a1f23409e5161e27cabc9beef5129929393e1f18
Gabriele <gabri.28.2015@gmail.com>
2020-04-24 14:52:55 +0200

    Visualizzazione dati cliente

--- src/main/java/it/objectmethod/ccespringboot/dao/IClienteDao.java
+++ src/main/java/it/objectmethod/ccespringboot/dao/IClienteDao.java
@@ -2,11 +2,8 @@ package it.objectmethod.ccespringboot.dao;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import it.objectmethod.ccespringboot.model.Cliente;
 
-@Service
 public interface IClienteDao {
 
 	public List<Cliente> getAllClienti();
